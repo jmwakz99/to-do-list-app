68d083bf49c9c4245fa8633eb76f7158fdadf7f6
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2021-01-22 21:34:26 +0300

    react folder architecture

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import ListItem from './components/ui/ListItem'
+
 
 class App extends React.Component {
 
   componentDidMount() {
-    // window.$('.ui.search')
-    //   .search({
-    //     source: 'Test'
-    //   })
-    //   ;
 
   }
   render() {
     return (
       <div className="App">
-        <ListItem></ListItem>
+
+
+
+
 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/rootReducers'
+import rootReducer from './store/reducers/rootReducers'
 
 const store = createStore(rootReducer)
 
